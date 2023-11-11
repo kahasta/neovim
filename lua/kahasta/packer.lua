469d4f32e57763e9bf730b4cf0339295d9023851
kahasta <kahastacold@gmail.com>
2023-11-11 22:07:15 +0400

    add autoinstall packer

--- lua/kahasta/packer.lua
+++ lua/kahasta/packer.lua
@@ -1,7 +1,20 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -228,12 +241,12 @@ return require('packer').startup(function(use)
             require('windows').setup()
         end
     }
-
-    use { 'echasnovski/mini.nvim', branch = 'stable',
-        config = function()
-            require('mini.animate').setup()
-        end
-    }
+    -- Animations cursor and etc
+    -- use { 'echasnovski/mini.nvim', branch = 'stable',
+    --     config = function()
+    --         require('mini.animate').setup()
+    --     end
+    -- }
     use { 'HiPhish/rainbow-delimiters.nvim', config = function()
     end }
     -- join in line code or unpack
